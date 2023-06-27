142343f4776f259d165441613c9346edff72289f
CrisArranz <c.arranz.arevalo@gmail.com>
2023-06-27 21:48:30 +0200

    feat. add obstacles

---
+++ assets/js/models/obstacle/obstacle.js
@@ -0,0 +1,7 @@
+class Obstacle {
+  constructor(context, positionX, positionY, width, height) {
+    this.context = context;
+    this.positionX = positionX;
+    this.positionY = positionY;
+  }
+}
