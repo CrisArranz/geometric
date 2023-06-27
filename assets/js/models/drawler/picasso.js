02bf97d5ad78e428ecec4858a9b149c836e13325
CrisArranz <c.arranz.arevalo@gmail.com>
2023-06-27 21:47:44 +0200

    feat. add new drawler

---
+++ assets/js/models/drawler/picasso.js
@@ -0,0 +1,27 @@
+class Picasso {
+  constructor(context, positionX, positionY, width, height, image) {
+    this.context = context;
+    this.positionX = positionX;
+    this.positionY = positionY;
+    this.width = width;
+    this.height = height;
+
+    this.image = new Image();
+    this.image.src = image;
+    this.image.onload = () => {
+      this.image.isReady = true;
+    }
+  }
+
+  draw() {
+    if (this.image.isReady) {
+      this.context.drawImage(
+        this.image,
+        this.positionX,
+        this.positionY,
+        this.width,
+        this.height
+      )
+    }
+  }
+}
