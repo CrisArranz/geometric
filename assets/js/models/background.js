e1f7c539dbcfde4db5142caf6b911b5d1efe322e
CrisArranz <c.arranz.arevalo@gmail.com>
2023-06-28 16:06:42 +0200

    feat. adding move method

--- assets/js/models/background.js
+++ assets/js/models/background.js
@@ -1,9 +1,12 @@
 class Background extends animatedSprite {
-  constructor(context) {
-    super(context, 0, 0, context.canvas.width * 2, context.canvas.height, "/assets/images/background.png")
+  constructor(context, positionX = 0, positionY = 0) {
+    super(context, positionX, positionY, context.canvas.width, context.canvas.height, "/assets/images/background.png");
   }
 
   move() {
     this.positionX -= BACKGROUND_IMAGE_SPEED_MODE;
+    if (this.positionX < -this.width) {
+      this.positionX = this.width - 1;
+    }
   }
 }
