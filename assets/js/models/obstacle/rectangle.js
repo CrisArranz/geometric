d5523a67fbe022a00e982da8de7e5742f79be0b9
CrisArranz <c.arranz.arevalo@gmail.com>
2023-06-28 17:51:35 +0200

    feat. add rectangles

--- assets/js/models/obstacle/rectangle.js
+++ assets/js/models/obstacle/rectangle.js
@@ -1,5 +1,10 @@
 class Rectangle extends Obstacle {
-  constructor(context, blocks) {
-    
+  constructor(context) {
+    super(context, context.canvas.width - 150, context.canvas.height - HEIGHT_FLOOR, WIDTH_OBSTACLE, HEIGHT_OBSTACLE, "/assets/images/block.png");
+    this.velocityX = OBSTACLE_VELOCITY;
+  }
+
+  move() {
+    this.positionX -= this.velocityX;
   }
 }
