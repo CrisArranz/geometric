dc081419d4da3e8a9ac8c640ac7bf2c39b76c019
CrisArranz <c.arranz.arevalo@gmail.com>
2023-06-30 09:34:18 +0200

    fix. add new attribute

--- assets/js/models/character/geometryc.js
+++ assets/js/models/character/geometryc.js
@@ -1,6 +1,7 @@
 class Geometryc extends animatedSprite {
   constructor(context) {
     super(context, GEOMETRYC_START_POSITION, context.canvas.height - HEIGHT_FLOOR, WIDTH_GEOMETRIC, HEIGHT_GEOMETRIC, "/assets/images/character.png");
+    this.positionY0 = context.canvas.height - HEIGHT_FLOOR;
     this.gravity = 1;
     this.velocityY = 0;
   }
@@ -8,15 +9,14 @@ class Geometryc extends animatedSprite {
   move() {
     this.velocityY += this.gravity;
     this.positionY += this.velocityY;
-    this.angle += 10;
 
-    if (this.positionY > this.context.canvas.height - HEIGHT_FLOOR) {
-      this.positionY = this.context.canvas.height - HEIGHT_FLOOR;
+    if (this.positionY > this.positionY0) {
+      this.positionY = this.positionY0;
     }
   }
 
   onKeyDown() {
-    if (this.positionY === this.context.canvas.height - HEIGHT_FLOOR) {
+    if (this.positionY === this.positionY0) {
       this.velocityY = -JUMP_VELOCITY;
     }
   }
