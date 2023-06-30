75d986aadc791afe7b079e592edeed1fa3e9baee
CrisArranz <c.arranz.arevalo@gmail.com>
2023-06-30 09:33:46 +0200

    fix. remove click method

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,19 +1,13 @@
 const game = new Game("geometric");
 
-window.addEventListener("keydown", () => {
-  game.onKeyDown();
-});
-
-window.addEventListener("click", () => {
-  if (game.intervalId) {
-    game.onKeyDown();
-  }
-});
-
 document.getElementById("btn-start").addEventListener("click",() => {
   game.start();
-})
+});
 
 document.getElementById("btn-stop").addEventListener("click",() => {
   game.stop();
-})
+});
+
+window.addEventListener("keydown", () => {
+  game.onKeyDown();
+});
