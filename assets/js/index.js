0294362387e46a9ee0e8d845df9907e4ba12a998
CrisArranz <c.arranz.arevalo@gmail.com>
2023-07-07 16:16:56 +0200

    feat. update controls

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,7 +1,14 @@
-const game = new Game("geometric");
-game.start();
+window.addEventListener("DOMContentLoaded", () => {
+  document.getElementById("start-btn").addEventListener("click", () => {
+    document.querySelector(".container-intro").classList.add("none");
+    document.querySelector(".container-game").classList.remove("none");
+    document.querySelector(".container-game").classList.remove("flex");
+    const game = new Game("geometric");
+    game.start();
 
-document.addEventListener("keydown", () => {
-  game.onKeyDown();
-});
+    document.addEventListener("keydown", () => {
+      game.onKeyDown();
+    });
+  })
+})
 
