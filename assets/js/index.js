d7beb9a79377c7210ef219afb96c5be118817d28
CrisArranz <c.arranz.arevalo@gmail.com>
2023-07-08 22:04:06 +0200

    feat. add audio intro

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,20 +1,34 @@
 window.addEventListener("DOMContentLoaded", () => {
+  document.querySelector("audio").volume = 0.1;
   document.getElementById("start-btn").addEventListener("click", () => {
-    document.querySelector(".container-intro").classList.add("none");
-    document.querySelector(".container-game").classList.remove("none");
-    document.querySelector(".container-game").classList.remove("flex");
-    const game = new Game("geometric", onGameOver);
-    game.start();
+    const audio = new Audio("/assets/audio/buttons.mp3");
+    audio.volume = 0.3;
+    audio.play();
+    document.querySelector("audio").volume = 0;
+    setTimeout(() => {
+      document.querySelector(".container-intro").classList.add("none");
+      document.querySelector(".container-game").classList.remove("none");
+      document.querySelector(".container-game").classList.remove("flex");
+      const game = new Game("geometric", onGameOver, onSaveProgress);
+      game.start();
 
-    document.addEventListener("keydown", () => {
-      game.onKeyDown();
-    });
+      document.addEventListener("keydown", () => {
+        game.onKeyDown();
+      });
+    }, 1000)
   })
 })
 
 
 function onGameOver() {
+  document.querySelector("audio").volume = 0.1;
   document.querySelector(".container-intro").classList.remove("none");
   document.querySelector(".container-game").classList.remove("flex");
   document.querySelector(".container-game").classList.add("none");
+}
+
+function onSaveProgress(progress) {
+  const arrayResults = window.localStorage.getItem("geometric-scores") ? JSON.parse(window.localStorage.getItem("geometric-scores")) : [];
+  arrayResults.push({score: progress})
+  window.localStorage.setItem("geometric-scores", JSON.stringify(arrayResults))
 }
